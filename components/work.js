8b55f9f569768880fddc174e7c85e1be74c91d0b
Sid Adam <122377134+Said2434@users.noreply.github.com>
2024-10-29 10:54:18 +0900

    Add files via upload

---
+++ components/work.js
@@ -0,0 +1,40 @@
+import NextLink from 'next/link'
+import { Heading, Box, Image, Link, Badge } from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+
+export const Title = ({ children }) => (
+  <Box>
+    <span>
+      {' '}
+      <ChevronRightIcon />{' '}
+    </span>
+    <Heading display="inline-block" as="h3" fontSize={20} mb={4}>
+      {children}
+    </Heading>
+  </Box>
+)
+
+export const TitleAI = ({ children }) => (
+  <Box>
+    <Link as={NextLink} href="/ai_projects">
+      AI
+    </Link>
+    <span>
+      {' '}
+      <ChevronRightIcon />{' '}
+    </span>
+    <Heading display="inline-block" as="h3" fontSize={20} mb={4}>
+      {children}
+    </Heading>
+  </Box>
+)
+
+export const WorkImage = ({ src, alt }) => (
+  <Image borderRadius="lg" w="full" src={src} alt={alt} mb={4} />
+)
+
+export const Meta = ({ children }) => (
+  <Badge colorScheme="green" mr={2}>
+    {children}
+  </Badge>
+)
