6be04c30e08ae898defa9b034260286a1a0b6559
Sid Adam <122377134+Said2434@users.noreply.github.com>
2024-11-14 16:23:12 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,42 +26,44 @@ import {
 import Image from 'next/image'
 
 const Home = () => (
-  <Layout>
-    <Container>
-    
-
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Sid Adam
-          </Heading>
-          <p>Data Scientist/Computer Vision Engineer </p>
-        </Box>
+<Layout>
+  <Container>
+    <Box display={{ md: 'flex' }} justifyContent="flex-start">
+      <Box flexGrow={1}>
+        <Heading as="h2" variant="page-title">
+          Sid Adam
+        </Heading>
+        <p>Data Scientist/Computer Vision Engineer</p>
+      </Box>
+      <Box
+        flexShrink={0}
+        mt={{ base: 4, md: 0 }}
+        ml={{ md: 0 }}  // Remove left margin to align it to the left
+        textAlign="left"  // Align the image to the left
+      >
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          textAlign="center"
+          borderColor="whiteAlpha.800"
+          borderWidth={2}
+          borderStyle="solid"
+          w="100px"
+          h="100px"
+          display="inline-block"
+          borderRadius="full"
+          overflow="hidden"
         >
-          <Box
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            w="100px"
-            h="100px"
-            display="inline-block"
-            borderRadius="full"
-            overflow="hidden"
-          >
-            <Image
-              src="/images/Adam.jpg"
-              alt="Profile image"
-              width="100"
-              height="100"
-            />
-          </Box>
+          <Image
+            src="/images/Adam.jpg"
+            alt="Profile image"
+            width="100"
+            height="100"
+          />
         </Box>
       </Box>
+    </Box>
+  </Container>
+</Layout>
+<Layout>
+
 
     <Section delay={0.1}>
   <Heading as="h3" variant="section-title">
